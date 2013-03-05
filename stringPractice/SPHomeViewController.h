a8a083aa1e691c89a2a68b36a2892c7c6528ef0a
KanebCenter <mrauh1@nd.edu>
2013-03-05 12:39:29 -0500

    Added view controller for home screen

---
+++ stringPractice/SPHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  SPHomeViewController.h
+//  stringPractice
+//
+//  Created by Learning Technology Lab on 3/5/13.
+//  Copyright (c) 2013 Learning Technology Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPHomeViewController : UIViewController
+
+@end
---
+++ stringPractice/SPHomeViewController.m
@@ -0,0 +1,38 @@
+//
+//  SPHomeViewController.m
+//  stringPractice
+//
+//  Created by Learning Technology Lab on 3/5/13.
+//  Copyright (c) 2013 Learning Technology Lab. All rights reserved.
+//
+
+#import "SPHomeViewController.h"
+
+@interface SPHomeViewController ()
+
+@end
+
+@implementation SPHomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
